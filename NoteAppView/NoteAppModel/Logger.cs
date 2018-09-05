46bae6a5e5ef338d7656db3e34b90a0e929f5bc3
sir Lenko <lenko.studio@gmail.com>
2018-09-05 20:19:58 +0700

    Класс для логгирования

---
+++ NoteAppView/NoteAppModel/Logger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace NoteAppModel
+{
+    public class Logger
+    {
+        public Logger()
+        {
+
+        }
+
+        #region Публичные методы
+        /// <summary>
+        /// Запись в консоль
+        /// </summary>
+        /// <param name="str">строка для логгирования</param>
+        public void Write(string str)
+        {
+            System.Diagnostics.Debug.WriteLine(GetLogString(str));
+        }
+
+        /// <summary>
+        /// Запись в консоль
+        /// </summary>
+        /// <param name="obj">Объект для логгирования</param>
+        public void Write(object obj)
+        {
+            if (obj != null)
+            {
+                System.Diagnostics.Debug.WriteLine(GetLogString(Newtonsoft.Json.JsonConvert.SerializeObject(obj)));
+            }
+        }
+        #endregion
+
+        #region Приватные методы
+
+        /// <summary>
+        /// Получение строки логгов
+        /// </summary>
+        /// <param name="str">Данные</param>
+        /// <returns>Запись</returns>
+        private string GetLogString(string str)
+        {
+            return $"LOG {DateTime.Now.ToLongTimeString()} : {str}";
+        }
+
+        #endregion
+    }
+}
