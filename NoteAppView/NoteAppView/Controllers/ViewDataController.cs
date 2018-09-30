ef8155a702ce2d54b5b180ff8796d0b1fb5e464e
sir Lenko <lenko.studio@gmail.com>
2018-09-30 08:36:17 +0700

    Добавление данных о пользователе

---
+++ NoteAppView/NoteAppView/Controllers/ViewDataController.cs
@@ -0,0 +1,26 @@
+﻿using NoteAppModel.Protocol;
+
+namespace NoteAppView
+{
+    public class ViewDataController
+    {
+        #region private
+        private static ViewDataController _instance;
+
+        #endregion
+
+        private ViewDataController() { }
+
+        public static ViewDataController GetInstance()
+        {
+            if (_instance == null)
+                _instance = new ViewDataController();
+            return _instance;
+        }
+
+        /// <summary>
+        /// Данные о пользователе
+        /// </summary>
+        public UserProtocol UserData { get; set; }
+    }
+}
